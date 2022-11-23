73e3e47af75223595d7c0975abf98c10a25cd40b
ok-onkarkhatode <118878794+ok-onkarkhatode@users.noreply.github.com>
2022-11-23 21:09:48 +0530

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,133 +1,72 @@
 /*
 #include<iostream>
 #include<conio.h>
-class Demo
-{
-  public:
-   int a;
-   int b;
-   Demo(int a,int b)
-     {
-       this->a=a;
-       this->b=b;
-     }
-  Demo* operator +(const Demo& other)
-     {
 
-       Demo* temp=(Demo*)malloc(sizeof(Demo));
-       temp->a=this->a + other.a;
-       temp->b=this->b + other.b;
+template<class T,class T1>
 
+	T add(T a,int b)
+	{
+	  return a+b;
+	
+	}
 
-    return temp;
-    }
-  Demo* operator - (const Demo& other)
-     {
+	int main()
+	{
+	 // std::cout<<add<int >(10,20)<<std::endl;
+	 //std::cout<<add<int ,char>(10,'a')<<std::endl;
+	  std::cout<<add<int ,double >(10,20.0)<<std::endl;
 
-       Demo* temp=(Demo*)malloc(sizeof(Demo));
-       temp->a=this->a - other.a;
-       temp->b=this->b - other.b;
+	_getch();
+	return 0;
+	}
+	*/
 
 
-    return temp;
-    }
-  Demo* operator * (const Demo& other)
-     {
-
-       Demo* temp=(Demo*)malloc(sizeof(Demo));
-       temp->a=this->a * other.a;
-       temp->b=this->b * other.b;
-
-
-    return temp;
-    }
-
-  Demo* operator / (const Demo& other)
-     {
-
-       Demo* temp=(Demo*)malloc(sizeof(Demo));
-       temp->a=this->a / other.a;
-       temp->b=this->b / other.b;
-
-
-    return temp;
-    }
-  Demo* operator % (const Demo& other)
-     {
-
-       Demo* temp=(Demo*)malloc(sizeof(Demo));
-       temp->a=this->a % other.a;
-       temp->b=this->b % other.b;
-
-
-    return temp;
-    }
-};
-int main()
-{
- Demo d1(10,20);
- Demo d2(20,30);
-// Demo *p=d1+d2; //addition opertor overloading
- // Demo *p=d1-d2;
-  Demo *p=d1*d2;
- // Demo *p=d1/d2;
- // Demo *p=d1%d2;
-
- Demo d3=*p;
- std::cout<<d3.a<<std::endl;
- std::cout<<d3.b<<std::endl;
- 
- _getch();
-return 0;
-}*/
-
-
+/*
+//template function
 #include<iostream>
 #include<conio.h>
 
 class Demo
 {
-  public:
-  int a;
-  int b;
-  
-
-   Demo(int a,int b)
-   {
-      this->a=a;
-	  this->b=b;
-   }
-  friend std::ostream& operator <<( std::ostream & os,const Demo & other);
-  friend std::istream& operator >>( std::istream & is,Demo & other);
-  Demo()
-  {
-  }
-
+public:
+	template<class T1,class T2>
+	T1 fun(T1 a,T2 b)
+	{
+	 return a+b;
+	}
 };
-
-std::ostream& operator <<( std::ostream & os,const Demo & other)
-{
-  os<<other.a<<std::endl;
-  os<<other.b<<std::endl;
-   return os;
-}
-
-std::istream& operator>>( std::istream& is,Demo& other)
-{
-  is>>other.a;
-  is>>other.b;
-   return is;
-}
 
 int main()
 {
- Demo d1(10,20);
- Demo d2(10,20);
- std::cout<<d1<<d2;
- Demo d3(40,50);
- std::cin>>d3;
-  std::cout<<d3<<std::endl;
+	Demo d;
+	std::cout<<d.fun<int ,int>(10 , 20)<<std::endl;
+	_getch();
+ return 0;
+}
+*/
 
- _getch();
-return 0;
+#include<iostream>
+#include<conio.h>
+
+template<class T>
+class Demo
+{
+  
+
+
+ T fun()
+{
+
+  
+}
+
+};
+
+int main()
+{
+
+	Demo<int,int>(10,20);
+	d.fun();
+ return 0;
 }
